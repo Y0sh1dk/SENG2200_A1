b5bdda12f31bf9675282aedaf29512d17270df79
Yosiah de Koeyer <yosiahdekoeyer@gmail.com>
2021-03-03 23:14:41 +1100

    Inital Polygon class, command line file arg

--- src/PA1.java
+++ src/PA1.java
@@ -1,10 +1,29 @@
+import java.nio.file.*;
+
 public class PA1 {
     public static void main(String[] args) {
-        PA1 main = new PA1();
-        main.run(args);
+        if (args.length != 1) { // If no args given, or too many args, exit
+            System.out.println("Usage: PA1 [file]");
+            return;
+        }
+        Path filePath = Paths.get(args[0]);
+
+        if (Files.exists(filePath)) {                               // If file exists, run
+            PA1 main = new PA1();
+            System.out.println("Using file: " + filePath);
+            main.run(filePath);
+        } else if (Files.exists(Paths.get((filePath + ".txt")))) {  // If it doesnt, try adding '.txt' extension
+            filePath = Paths.get((filePath + ".txt"));
+            PA1 main = new PA1();
+            System.out.println("Using file: " + filePath);
+            main.run(filePath);
+        } else {                                                    // else no file can be found, exit
+            System.out.println("File " + filePath.getFileName() + " is not found");
+            System.out.println("Exiting...");
+        }
     }
 
-    private void run(String[] args) {
+    private void run(Path filePath) {
         Point p = new Point(345.564, 87.111);
         System.out.println(p);
         System.out.println(p.distFromOrigin());
--- src/Polygon.java
+++ src/Polygon.java
@@ -1,2 +1,36 @@
-public class Polygon {
+import java.util.ArrayList;
+
+public class Polygon implements ComparePoly {
+    private static final int minNumOfSides = 3; // Polygon cant have less than 3 sides yo
+    private ArrayList<Point> points;
+    private int numOfPoints;
+
+    Polygon() {
+        this.points = new ArrayList<>();
+        this.numOfPoints = 0;
+    }
+
+    public void addPoint(double inXCoord, double inYCoord) {
+        this.points.add(new Point(inXCoord, inYCoord));
+        this.numOfPoints++; // increment number of points
+    }
+
+    public boolean isValid() {
+        return this.numOfPoints >= 3; // returns true of 3 or more points, else returns false
+    }
+
+
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public boolean ComesBefore(Polygon p) {
+        return false; //TODO: this
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
